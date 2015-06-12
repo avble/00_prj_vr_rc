1263cb11be1dc6ccc13e38b632dc99b3aaa1b3e7
john le <huy.le@veriksystems.com>
2015-06-12 13:37:23 +0700

    start managing server

---
+++ 00_server/venusserver.py
@@ -0,0 +1,27 @@
+from flask import Flask
+app = Flask(__name__)
+
+#@app.route('/helloworld/')
+#def helloworld():
+#	return "hello world" 
+
+@app.route('/')
+def index():
+    return 'index page' 
+
+@app.route('/helloworld', methods = ['POST', 'GET'])
+def helloworld():
+    return 'hello world' 
+
+@app.route('/device/registerDevice', methods = ['POST']
+def device_register():
+    return 'register device' 
+
+@app.route('/device/resetDevice', methods = ['POST'])
+def device_reset():
+    # show the post with the given id, the id is an integer
+    return 'reset device' 
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0')
